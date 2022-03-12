0106ee74d5963d8f1adcf0cd9a63391de780a8b5
shuto uehara <shuto.u2f@gmail.com>
2022-03-12 17:50:24 +0900

    modified contact text

--- src/components/contact-btn.js
+++ src/components/contact-btn.js
@@ -6,7 +6,7 @@ export default function ContactBtn () {
   return (
     <div className="text-center my-5 py-5">
       <div className="d-flex justify-content-center align-items-center">
-        <span>サイトの制作・改修などを依頼</span>&nbsp;
+        <span>サイトの制作・改修などを<br />気になってることを質問</span>&nbsp;
         <img src={letter} alt="問い合わせボタンエリア" width="40" height="32" className="letter" />
       </div>
       <BtnOriginal btnText="お問い合わせはこちら" linkTo="/contact/" btnClass="mt-3 mb-5 py-2" />
