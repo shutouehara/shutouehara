dba41dd9e8261f76cce740a353d12821ab1c667d
shuto uehara <shuto.u2f@gmail.com>
2022-03-20 00:32:51 +0900

    updated

--- src/components/card.js
+++ src/components/card.js
@@ -4,7 +4,7 @@ import { Card } from "react-bootstrap"
 const CardOriginal = ({ linkParent, linkTo, imgSrc, title, description, moreText }) => {
   return (
     <Card class="card-group" style={{ width: '18rem' }} as="a" href={`/${linkParent}/${linkTo}/`}>
-      <div className="position-relative overflow-hidden" style={{ height: '12rem' }} >
+      <div className="position-relative overflow-hidden" style={{ width: '100%', height: '12rem' }} >
         <Card.Img variant="top" src={imgSrc} className="position-absolute" style={{ top: '50%', transform: 'translateY(-50%)' }} />
       </div>
       <Card.Body>
