49fec5373f509401f63597f232a4981c40b431e8
shuto uehara <shuto.u2f@gmail.com>
2022-03-30 21:11:17 +0900

    modified

--- src/pages/blogs/{microcmsBlog.blogId}.js
+++ src/pages/blogs/{microcmsBlog.blogId}.js
@@ -49,17 +49,6 @@ const BlogPage = ({ pageContext, data }) => {
             className="pt-3 pt-md-5 lh-lg article-inner"
           />
         </article>
-        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6716641662780805"
-     crossorigin="anonymous"></script>
-        <ins class="adsbygoogle"
-            style="display:block"
-            data-ad-client="ca-pub-6716641662780805"
-            data-ad-slot="5179379727"
-            data-ad-format="auto"
-            data-full-width-responsive="true"></ins>
-        <script>
-            (adsbygoogle = window.adsbygoogle || []).push({});
-        </script>
         <ContactBtn />
       </div>
       <div className="my-5 text-center">
