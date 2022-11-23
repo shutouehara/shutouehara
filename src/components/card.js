d70a6b8ffcc14d56df1ed025377e866940a9cf6f
shuto uehara <shuto.u2f@gmail.com>
2022-11-23 14:27:42 +0900

    modified styles

--- src/components/card.js
+++ src/components/card.js
@@ -7,7 +7,7 @@ const CardOriginal = ({ linkParent, linkTo, imgSrc, title, time, category, descr
       <div className="position-relative overflow-hidden" style={{ width: '100%', height: '12rem' }} >
         <Card.Img variant="top" src={imgSrc} className="position-absolute" style={{ top: '50%', transform: 'translateY(-50%)' }} />
       </div>
-      <Card.Body>
+      <Card.Body className="p-3">
         <Card.Title as="h2" className="fw-bold h4 mb-0">{title}</Card.Title>
         <Card.Text className="mt-1 mb-2 me-2 d-inline-block fs-small">{time}&nbsp;更新</Card.Text>
         <Card.Text className="mt-1 mb-2 px-1 d-inline-block blog-category fw-light fs-small">{category}</Card.Text>
